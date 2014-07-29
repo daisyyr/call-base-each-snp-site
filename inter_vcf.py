81dbcc3e39ac1479302c2ca18010baef26da9d07
393099545@qq.com <cmiao@apple.(none)>
2014-07-29 10:01:03 +0800

    update

--- inter_vcf.py
+++ inter_vcf.py
@@ -35,9 +35,10 @@ def common_vcf(x, y, z):
     common_set = vf1_set & vf2_set & vf3_set
     rf = open('common_sites.vcf', 'w')
     for i in open(x, 'r'):
-        obj = i.split()[0] + '-' + i.split()[1]
-        if obj in common_set:
-            rf.write(i)
+        if not x.startswith('#')
+            obj = i.split()[0] + '-' + i.split()[1]
+            if obj in common_set:
+                rf.write(i)
     rf.close()
 
 if __name__ == '__main__':
